efb12d792f229f3bdd8f356cfda82b13e785e837
Chronos410 <chronos410@naver.com>
2025-05-21 09:01:04 +0900

    12주차 실습

---
+++ WorkFile/12th/12th/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Sockets;
+
+using System.Text;
+
+namespace _12th
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sock = null;
+
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            string SVR_IP = "127.0.0.1";
+            int SVR_PORT = 9000;
+
+            sock.Connect(SVR_IP, SVR_PORT);
+
+            byte[] SendData = Encoding.UTF8.GetBytes("Hi Im Youngsu");
+            int sizeData = SendData.Length;
+
+            sock.Send(SendData, 0, sizeData, SocketFlags.None);
+
+            sock.Close();
+            Console.Read();
+            
+
+        }
+    }
+}
